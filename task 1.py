1ff0afc3b67fe81b2298593c5e5f9bc4a8c9f847
Akakhilesh <akakhilesh121@gmail.com>
2026-02-22 20:02:51 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,13 @@
+def factorial(num):
+    '''User-defines function to find the factorial of a number
+    :arg num: number to find its factorial
+    :returns: the factorial of num'''
+
+    factorial_num=1                          # Stores the factorial of the number
+    while num>0:
+        factorial_num=factorial_num*num
+        num-=1
+    return factorial_num                     # Returns the Factorial_num when calling
+
+user_input=int(input("Enter a number to Finds its factorial: "))
+print(f"The factorial of {user_input} is {factorial(user_input)}")  # printing the called factorial function
---
+++ task 2.py
@@ -0,0 +1,6 @@
+import math
+number = int(input("Enter a number: "))             # takes number as integer input
+print(f"Square root : {math.sqrt(number)}")         # displays square root of the number
+print(f"Logarithm : {math.log(number)}")            # displays logarithm of the number
+print(f"Sine : {math.sin(number)}")                 # displays sine of the number in radians
+
